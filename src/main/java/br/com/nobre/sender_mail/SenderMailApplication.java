2424b82685cd585e1d74b064229d6e7bab1a4357
BrenoHenrique07 <brenohenrique.developer@gmail.com>
2025-02-15 20:31:52 -0300

    Inital commit

---
+++ src/main/java/br/com/nobre/sender_mail/SenderMailApplication.java
@@ -0,0 +1,13 @@
+package br.com.nobre.sender_mail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SenderMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SenderMailApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/nobre/sender_mail/SenderMailApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.nobre.sender_mail;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SenderMailApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
